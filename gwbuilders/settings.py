54b5559811aec02c0d074fec0369196bb92e4a34
Hans Barredo <hansit.barredo@gmail.com>
2024-12-18 07:52:53 +0800

    settings update

--- gwbuilders/settings.py
+++ gwbuilders/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,6 +86,10 @@ DATABASES = {
         'PASSWORD': 'asus1234',
         'HOST': 'localhost'
     }
+}
+
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
