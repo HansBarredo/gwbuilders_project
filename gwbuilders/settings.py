eb283d04eed3ef5822782a17daaf099769060805
Hans Barredo <hansit.barredo@gmail.com>
2024-12-17 23:39:43 +0800

    settings

--- gwbuilders/settings.py
+++ gwbuilders/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,8 +86,6 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-
-DATABASES ['default'] =dj_database_url.parse("postgresql://gwbuilders_user:SsdsPrQBgpljBMWsQcBg3HXUodV7mS6s@dpg-ctgobi56l47c73e8uccg-a.singapore-postgres.render.com/gwbuilders")
 
 
 # Password validation
