fdb1e420d74cb1b6b8804fc933a5c75482d378a1
Hans Barredo <hansit.barredo@gmail.com>
2024-12-17 23:26:56 +0800

    settings update

--- gwbuilders/wsgi.py
+++ gwbuilders/wsgi.py
@@ -14,3 +14,16 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gwbuilders.settings')
 
 application = get_wsgi_application()
+
+
+import os
+import sys
+
+# Ensure the virtual environment is used
+activate_this = '/opt/render/project/src/.venv/bin/activate_this.py'
+exec(open(activate_this).read(), {'__file__': activate_this})
+
+# Your standard Django wsgi setup
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gwbuilders.settings')
+application = get_wsgi_application()
