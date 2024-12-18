5abf6006c5b75b250fc5182d47baa5c8ad8033ec
Hans Barredo <hansit.barredo@gmail.com>
2024-12-18 08:14:50 +0800

    white noise added static updated

--- gwbuilders/settings.py
+++ gwbuilders/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'gwbuilders.urls'
@@ -120,6 +121,14 @@ USE_TZ = True
 
 STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'gwbuilders/assets')
 ]
